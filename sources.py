3212d50cf385e394089c7594488897ec55ad1213
whatever <nada>
2016-10-29 16:20:21 +0000

    more nanc sources

--- sources.py
+++ sources.py
@@ -2,37 +2,38 @@ from enum import Enum
 
 class Source(Enum):
     AFP = ([], 'Agence France-Presse')
+    APW = ([], 'Associated Press Worldstream')
     ALB_TIMES_U = ([], 'Albany Times Union')
     ARZ_REPUB = (['ARIZONA-REPUBLIC', 'AZR'], 'Arizona Republic')
     ATL_CONST = ([], 'Atlanta Constitution')
     BLMBRG = (['BLOOM'], 'Bloomberg Business News')
-    BOST = (['BO', 'BOS'], 'Boston Globe')
+    BOST = (['B', 'BO', 'BOS'], 'Boston Globe')
     CASPER = ([], 'Casper (Wyo.) Star-Tribune')
+    CHRON = ([], 'Houston Chronicle') # no unique tag
     CHI_SUN = ([], 'Chicago Sun-Times')
     COLUMBIA_NEWS = ([], 'Columbia News Service')
     COX = (['COX'], 'Cox News Service')
-	ECO = (['EC', 'ECO', 'ECONOMI', 'ECONOMIST'], 'Economist')
+    CNA = ([], 'Central News Agency of Taiwan')
+    ECO = (['EC', 'ECO', 'ECONOMI', 'ECONOMIST'], 'Economist')
     FW_STAR_TELEGRAM = (['TE', 'TEX'], 'Fort Worth Star-Telegram')
-    HRST = ([], 'Hearst Newspapers') # no unique tag
-    CHRON = ([], 'Houston Chronicle') # no unique tag
+    HRST = (['HNS'], 'Hearst Newspapers') # no unique tag
     INTL_HERALD_TRIB = ([], 'International Herald Tribune')
     KAN_CITY_STAR = (['KAN'], 'Kansas City Star')
     LA_DAILY = (['LADN'], 'Los Angeles Daily News')
-    SAN_ANTONIO_EXPRESS = ([], 'San Antonio Express-News') # no unique tag
-    SF_CHRON = (['San Francisco Chronicle', 'SFCHRON'], 'San Francisco Chronicle')
-    SEATTLE_POST_INTEL = (['SPI'], 'Seattle Post-Intelligencer')
-    ST_NEWS_SERVICE = ([], 'State News Service')
-    APW = ([], 'Associated Press Worldstream')
-    CNA = ([], 'Central News Agency of Taiwan')
     LAT = (['LAT'], 'Los Angeles Times')
     LATW = ([], 'LA Times/WaPo Newswire')
     LBPT = ([], 'Long Beach Press-Telegram')
-    WAPO = ([], 'Washington Post')
-	WSJ = ([], 'Wall-Street Journal')
     NYT = (['NYT'], 'New York Times')
+    REUTE = (['REUFF', 'REUTE'], 'Reuters')
+    SAN_ANTONIO_EXPRESS = ([], 'San Antonio Express-News') # no unique tag
+    SEATTLE_POST_INTEL = (['SPI'], 'Seattle Post-Intelligencer')
+    SF_CHRON = (['San Francisco Chronicle', 'SFCHRON'], 'San Francisco Chronicle')
+    SFE = ([], 'San Francisco Examiner') # no unique tag
     SLATE = ([], 'Slate')
+    ST_NEWS_SERVICE = ([], 'State News Service')
+    WAPO = ([], 'Washington Post')
+    WSJ = ([], 'Wall-Street Journal')
     XIN = ([], 'Xinua News Agency')
-	REUTE = (['REUFF', 'REUTE'], 'Reuters')
 
     def __init__(self, datelines, name):
         self.datelines = datelines
