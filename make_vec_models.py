44d0fd60a9a4ff636a9a61888c8705eb446b0b30
whatever <nada>
2016-10-30 03:42:45 +0000

    make it better

--- make_vec_models.py
+++ make_vec_models.py
@@ -8,24 +8,27 @@ from something import BigIterable
 from sources import Source
 
 def get_source_file_path(temp_path, source):
-    file_name = re.sub(r'[^a-zA-Z]', '_', source.source_name + '.txt')
+    file_name = re.sub(r'[^a-zA-Z]', '_', source.source_name) + '.txt'
     return os.path.join(temp_path,  file_name)
 
 def make_vec_models(save_path, temp_path, *extractors):
     all_articles = BigIterable(*extractors)
     source_file_map = dict()
-    for source in Source:
-        file_path = get_source_file_path(temp_path, source)
-        source_file_map[source] = open(file_path, 'w')
     try:
         for article in all_articles:
+            if article.source in source_file_map:
+                source_file = source_file_map[article.source]
+            else:
+                file_path = get_source_file_path(temp_path, article.source)
+                source_file = open(file_path, 'w')
+                source_file_map[article.source] = source_file
             text = article.text
             if article.headline:
                 text = ' '.join([article.headline, text])
             text = text.replace('\n', ' ')
             text = ' '.join(re.sub(r'[^a-zA-Z ]', '', text).lower().split())
-            source_file_map[source].write(text)
-            source_file_map[source].write('\n')
+            source_file.write(text)
+            source_file.write('\n')
     except:
         for f in source_file_map.values():
             f.close()
@@ -36,9 +39,8 @@ def make_vec_models(save_path, temp_path, *extractors):
 
     NUM_PASSES = 5
 
-    for source in Source:
+    for source in source_file_map.keys():
         print('training on %s' % source)
-        
         file_path = get_source_file_path(temp_path, source)
         sentences = gensim.models.word2vec.LineSentence(file_path)
         model = gensim.models.Word2Vec(sentences,
