69d9af40e0c50196099f27786ecec5258d9ea79a
whatever <nada>
2016-10-30 22:10:28 +0000

    DataManager

--- make_vec_models.py
+++ make_vec_models.py
@@ -7,52 +7,105 @@ import re
 from something import BigIterable
 from sources import Source
 
-def get_source_file_path(temp_path, source):
-    file_name = re.sub(r'[^a-zA-Z]', '_', source.source_name) + '.txt'
-    return os.path.join(temp_path,  file_name)
 
-def generate_temp_corp_files(temp_path, *extractors):
-    all_articles = BigIterable(*extractors)
-    source_file_map = dict()
-    try:
-        for article in all_articles:
-            if article.source in source_file_map:
-                source_file = source_file_map[article.source]
-            else:
-                file_path = get_source_file_path(temp_path, article.source)
-                source_file = open(file_path, 'w')
-                source_file_map[article.source] = source_file
-            text = article.text
-            if article.headline:
-                text = ' '.join([article.headline, text])
-            text = text.replace('\n', ' ')
-            text = ' '.join(re.sub(r'[^a-zA-Z ]', '', text).lower().split())
-            source_file.write(text)
-            source_file.write('\n')
-    except:
+class DataManager:
+    def __init__(self, temp_path, save_path):
+        self.temp_path = temp_path
+        self.save_path = save_path
+
+    def clear_files(self, path):
+        for f in os.listdir(path):
+            f_path = os.path.join(path, f)
+            if os.path.isfile(f_path):
+                os.unlink(f_path)
+
+    def clear_temp_files(self):
+        '''
+        Deletes temp files. Good to use before regenerating them to
+        ensure you are not surprised by a file left behind
+        '''
+        self.clear_files(self.temp_path)
+
+    def clear_model_files(self):
+        '''
+        Deletes model files. Good to use before regenerating models 
+        to ensure you are not surprised by a model left behind.
+        '''
+        self.clear_files(self.save_path)
+
+    def get_file_path(self, path, source):
+        file_name = re.sub(r'[^a-zA-Z]', '_', source.source_name) + '.txt'
+        return os.path.join(path,  file_name)
+
+    def get_text_file_path(self, source):
+        return self.get_file_path(self.temp_path, source)
+
+    def get_model_file_path(self, source):
+        return self.get_file_path(self.save_path, source)
+
+    def generate_extractions(self, *extractors):
+        '''
+        Generates temp files with text extractions for every source
+        found in articles from *extractors, iterables which 
+        return instances of class get_articles.Article
+
+        Returns set of sources extracted
+        '''
+        all_articles = BigIterable(*extractors)
+        source_file_map = dict()
+        try:
+            for article in all_articles:
+                if article.source in source_file_map:
+                    source_file = source_file_map[article.source]
+                else:
+                    file_path = self.get_text_file_path(source)
+                    source_file = open(file_path, 'w')
+                    soruce_file_map[article.source] = source_file
+                text = article.text
+                if article.headline:
+                    text = ' '.join([article.headline, text])
+                text = text.replace('\n', ' ')
+                text = ' '.join(re.sub(r'[^a-zA-Z ]', '', text).lower().split())
+                source_file.write(text)
+                source_file.write('\n')
+        except:
+            for f in source_file_map.values():
+                f.close()
+            raise Exception('Article text extraction failed. Bailing out.')
+
         for f in source_file_map.values():
             f.close()
-        raise Exception('Article text extraction failed. Bailing out.')
 
-    for f in source_file_map.values():
-        f.close()
+        return source_file_map.keys()
 
-    return source_file_map.keys()
+    def generate_model_files(self, sources, gensim_args=None):
+        default_kwargs = {
+            'iter': 5,
+            'workers': multiprocessing.cpu_count()
+        }
 
-def generate_model_files(save_path, temp_path, sources, num_passes=5):
-    for source in sources:
-        print('training on %s' % source)
-        file_path = get_source_file_path(temp_path, source)
-        sentences = gensim.models.word2vec.LineSentence(file_path)
-        model = gensim.models.Word2Vec(sentences,
-                                       iter=num_passes, 
-                                       workers=multiprocessing.cpu_count())
-        model_path = get_source_file_path(save_path, source)
-        model.save(model_path)
+        if gensim_args:
+            assert(type(gensime_args) == dict)
+            default_kwargs.update(gensim_args)
 
-def make_vec_models(save_path, temp_path, *extractors):
-    temp_file_sources = generate_temp_corp_files(temp_path, *extractors)
-    generate_model_files(save_path, temp_path, temp_file_sources) 
+        for source in sources:
+            print('Training on %s' % source)
+            f_path = self.get_text_file_path(source)
+            sentences = gensim.models.word2vec.LineSentence(f_path)
+            model = gensim.models.Word2Vec(sentences,
+                                           **default_kwargs)
+            model_path = self.get_model_file_path(source)
+            model.save(model_path)
+
+    def clear_all_and_make_models(self, *extractors, gensim_args=None):
+        '''
+        Clears the temp directory and the model directory
+        and starts from scratch
+        '''
+        self.clear_temp_files()
+        self.clear_model_files()
+        sources = self.generate_extractions(*extractors)
+        self.generate_model_files(sources)
 
 def judge_model_qualities(save_path, questions_path):
     all_accur_data = dict()
