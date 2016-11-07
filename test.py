3133b289d643357fb2b0c02a38702a8599b4604e
whatever <nada>
2016-11-07 01:17:47 +0000

    cleaning up tests

--- test.py
+++ test.py
@@ -14,13 +14,4 @@ def test_extractors(*extractors_and_paths):
 if __name__ == '__main__':
     #test_extractors((AquaintDatasetExtractor,'/home/paper/aquaint_real'))
     #test_extractors((GigawordDatasetExtractor,'/newsdata/gigaword/'))
-    '''
-    test_extractors((NANCDatasetExtractor,'/home/paper/nanc/LDC2008T15__North_American_News_Text_Complete_NTC/latwp'))
-    test_extractors((NANCDatasetExtractor,'/home/paper/nanc/LDC2008T15__North_American_News_Text_Complete_NTC/reute'))
-    test_extractors((NANCDatasetExtractor,'/home/paper/nanc/LDC2008T15__North_American_News_Text_Complete_NTC/reuff'))
-    test_extractors((NANCDatasetExtractor,'/home/paper/nanc/LDC2008T15__North_American_News_Text_Complete_NTC/wsj'))
-    test_extractors((NANCDatasetExtractor,'/home/paper/nanc/LDC2008T15__North_American_News_Text_Complete_NTC/nyt'))
-    '''
-    #test_extractors((NANCDatasetExtractor,'/home/paper/nanc/LDC2008T15__North_American_News_Text_Complete_NTC/latwp/1994/lw940623'))
-    #test_extractors((NANCDatasetExtractor,'/home/paper/nanc/LDC2008T15__North_American_News_Text_Complete_NTC'))
-    test_extractors((NANCDatasetExtractor,'/home/paper/extractors/nanc-subset'))
+    test_extractors((NANCDatasetExtractor,'/home/paper/nanc/LDC2008T15__North_American_News_Text_Complete_NTC'))
