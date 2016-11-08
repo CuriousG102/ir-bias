75f30cd1e5249293da6236697ebc521374ef1067
Ubuntu <paper2@news2.iavh2i2zwdwutfoyviho11fhra.gx.internal.cloudapp.net>
2016-11-08 05:02:17 +0000

    bug fix

--- extract.py
+++ extract.py
@@ -1,13 +1,13 @@
 from aquaint import AquaintDatasetExtractor
 from gigaword import GigawordDatasetExtractor
 from model_data import DataManager
-from nanc import NANCDatasetExtractor
+from nanc_soup import NANCDatasetExtractor
 from settings import settings 
 
 dm = DataManager(settings['new_temp_path'], settings['save_path'])
 dm.generate_extractions(
     AquaintDatasetExtractor('/newsdata/aquaint_real'),
-    GigawordDatasetExtractor('/newsdata/gigaword/'),
-    NANCDatasetExtractor('/newsdata/nanc/')
+    GigawordDatasetExtractor('/newsdata/gigaword'),
+    NANCDatasetExtractor('/newsdata/nanc/LDC2008T15__North_American_News_Text_Complete_NTC')
 )
 
--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ settings = {
     'experiment_save_path': '/home/paper/extractors/experiment_models',
     'golden_model_path': '/newsdata/google_news/GoogleNews-vectors-negative300.bin',
     'positive_words_path': '/newsdata/gender_specific.csv',
-    'word_pairs_path': '/home/paper2/extractors/word_pairs.csv'
+    'word_pairs_path': '/home/paper2/extractors/word_pairs.csv',
 	'new_temp_path': '/newsdata/new_extracted_text',
     'save_path': '/home/paper2/ir-bias/models',
     'questions_path': '/home/paper2/ir-bias/questions-words.txt'
