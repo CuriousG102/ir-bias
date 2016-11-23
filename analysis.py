cef6edfbae88f91dfad66b92e3bee89e2a411baa
Ubuntu <paper3@news3.ut3mkiehwejuzdnqedco12cr1c.gx.internal.cloudapp.net>
2016-11-23 02:31:56 +0000

    Updated gender neutral word methodology and settings

--- analysis.py
+++ analysis.py
@@ -1,25 +1,159 @@
+import csv
+import pickle
+from functools import lru_cache
+
 import gensim
-import numpy
+from gensim.models import Word2Vec
+import numpy as np
+from sklearn import svm, model_selection
+from sklearn.decomposition import PCA
 
-from sources import Source
+from explore import judge_model_quality
 from model_data import DataManager
+from settings import settings
 
-TEMP = '/newsdata/models_test'
-SAVE = '/home/paper/extractors/models/'
+class BiasFinder:
+    '''
+    Probably not thread safe
+    '''
+    def __init__(self, train_cutoff=50000,
+                 dm_temp_path=settings['temp_path'],
+                 dm_save_path=settings['experiment_save_path'],
+                 accuracy_cutoff=.4,
+                 golden_model_path=settings['golden_model_path'],
+                 positive_words_path=settings['positive_words_path'],
+                 word_filter=lambda w: w.isalpha() and len(w) <= 20,
+                 word_pairs_path=settings['word_pairs_path'],
+                 accuracy_path="/newsdata/models_temp_data/",
+                 models_acc_name = "models_accuracy2"):
+        self.train_cutoff = train_cutoff
+        self.data_manager = DataManager(dm_temp_path, dm_save_path)
+        self.accuracy_cutoff = accuracy_cutoff
+        self.golden_model_path = golden_model_path
+        self.positive_words_path = positive_words_path
+        self.word_filter = word_filter
+        self.word_pairs_path = word_pairs_path
+        self.accuracy_path = accuracy_path
+        self.models_acc_name = models_acc_name
 
-'''playing with what  calculations of word embeddings will look like'''
+    def normalize(self, vector):
+        return vector/np.linalg.norm(vector)
 
-def analysis():
-    data_manager = DataManager(TEMP, SAVE)
-    model = data_manager.get_model_for_source(getSource('SLATE')) 
-    print(numpy.linalg.norm(normalize(model['woman'])))
+    def save_model_accuracy(self):
+        source_acc = {} 
+        name = self.models_acc_name
+        for source in self.data_manager.get_available_source_models():
+            print('Judging accuracy for %s' % source)
+            model = self.data_manager.get_model_for_source(source)
+            accur_dict, accur_percentage = judge_model_quality(model)
+            source_acc[source] = accur_dict, accur_percentage
+        with open(self.accuracy_path + name + '.pkl', 'wb') as f:
+            pickle.dump(source_acc, f)
+        
+    def load_model_accuracy(self):
+        with open(self.accuracy_path + self.models_acc_name + '.pkl', 'rb') as f:
+            return pickle.load(f)
+        
+    @lru_cache()
+    def get_eligible_sources(self):
+        sources = []
+        source_acc = self.load_model_accuracy()
+        for source in source_acc:
+            accur_dict, accur_percentage = source_acc[source]
+            print('Accuracy: %f' % accur_percentage)
+            if accur_percentage >= self.accuracy_cutoff:
+                sources.append(source)
+        return sources
 
-def normalize(word):
-    mag = numpy.linalg.norm(word)
-    return word/mag
+    def top_words_by_count(self, model, use_train_cutoff=False):
+        if use_train_cutoff:
+            words_by_count = sorted(model.vocab.items(),
+                                    key=lambda x: x[1].count,
+                                    reverse=True)
+        else:
+            words_by_count = model.vocab.items()
+        if use_train_cutoff:
+            top_words = words_by_count[:self.train_cutoff]
+        else:
+            top_words = words_by_count
+        top_words = [w for w, attr in top_words if self.word_filter(w)]
+        return top_words
 
-def getSource(abrv):
-    '''hard coded for now...'''
-    return Source.SLATE
+    @lru_cache()
+    def top_words_by_source(self, source, use_train_cutoff):
+        print('Getting top words for %s' % source)
+        source_model = self.data_manager.get_model_for_source(source)
+        return self.top_words_by_count(source_model,
+                                       use_train_cutoff=use_train_cutoff)
 
-analysis()
+    @lru_cache()
+    def top_words_golden(self, use_train_cutoff):
+        print('Getting top words for Golden')
+        golden_model = Word2Vec.load_word2vec_format(self.golden_model_path,
+                                                     binary=True)
+        top_words = self.top_words_by_count(golden_model, 
+                                            use_train_cutoff=use_train_cutoff)
+        top_words = set([w.lower() for w in top_words])
+        return top_words
+
+    @lru_cache()
+    def get_positive_words(self):
+        with open(self.positive_words_path) as f:
+            return [row[0].strip() for row in csv.reader(f)]
+
+    @lru_cache()
+    def get_common_positive_words(self):
+        top_golden_words = self.top_words_golden(True)
+        
+        positive_words = self.get_positive_words()
+        positive_words = set(top_golden_words) & set(positive_words)
+
+        for source in self.get_eligible_sources():
+            positive_words &= set(self.top_words_by_source(source, False))
+
+        return positive_words
+
+    @lru_cache()
+    def get_common_neutral_words(self):
+        #top_golden_words = self.top_words_golden(True)
+        #neutral_words = top_golden_words - set(self.get_common_positive_words())
+        with open('/newsdata/crawford_gend_neutral.csv') as f:
+            neutral_words = [row[0].strip() for row in csv.reader(f)]
+        neutral_words = set(neutral_words)
+
+        for source in self.get_eligible_sources():
+            neutral_words &= set(self.top_words_by_source(source, False))
+
+        return neutral_words
+
+    def get_source_bias_measurement(self, source, c=1.0):
+        w2v_model = self.data_manager.get_model_for_source(source)
+        with open(self.word_pairs_path) as f:
+            pair_words = []
+            for she_word, he_word in csv.reader(f):
+                if she_word in w2v_model and he_word in w2v_model:
+                    pair_words.append((she_word, he_word))
+
+        embed_diffs = [] 
+        for she_word, he_word in pair_words:
+            she_embed = self.normalize(w2v_model[she_word])
+            he_embed = self.normalize(w2v_model[he_word])
+            embed_diff = she_embed - he_embed
+            embed_diffs.append(embed_diff)
+        pca = PCA(n_components=len(embed_diffs))
+        pca.fit(embed_diffs)
+        # pc is g in the paper
+        pc = self.normalize(pca.components_[0])
+        # neutral words is N in the paper
+        neutral_words = (list(self.get_common_neutral_words()))
+        tot_direct_bias = 0.0
+        # word is w in paper
+        for word in neutral_words:
+            # w_vec in paper
+            word_embedding = self.normalize(w2v_model[word])
+            tot_direct_bias += abs(np.dot(pc, word_embedding)) ** c
+        
+        tot_direct_bias /= len(neutral_words)
+        print(tot_direct_bias)
+        return tot_direct_bias
+
--- settings.py
+++ settings.py
@@ -7,10 +7,10 @@ settings = {
     'experiment_save_path': '/newsdata/experiment_models',
     'golden_model_path': '/newsdata/google_news/GoogleNews-vectors-negative300.bin',
     'positive_words_path': '/newsdata/gender_specific.csv',
-    'word_pairs_path': '/home/paper2/ir-bias/word_pairs.csv',
+    'word_pairs_path': '/home/paper3/ir-bias/word_pairs.csv',
     'new_temp_path': '/newsdata/new_extracted_text',
     'save_path': '/home/paper2/ir-bias/models',
-    'questions_path': '/home/paper2/ir-bias/questions-words.txt'
+    'questions_path': '/home/paper3/ir-bias/questions-words.txt'
 }
 
 for k in settings:
